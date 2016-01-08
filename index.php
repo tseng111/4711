86627d6365718ffd408c5b5c616d4af64117d0f9
a00926085 <a00926085@SE12L0310-09.ad.bcit.ca>
2016-01-07 16:36:32 -0800

    Step 5: Here we go.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+		echo 'Hello World';
+		echo 'hi there';
+                $temp = 'Jim';
+                echo 'Hi, my name is';
+                echo $temp;
+                $temp = 'geek';
+                echo "I am a";
+                echo$temp;
+                $temp = 10;
+                echo 'My level is';
+                echo $temp;
+		?>
+	</body>
+</html>
