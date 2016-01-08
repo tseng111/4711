3eec3bd25e232390bfd722489815346f638f7ec0
a00926085 <a00926085@SE12L0310-09.ad.bcit.ca>
2016-01-07 17:19:46 -0800

    Step 5: Here we go.

--- index.php
+++ index.php
@@ -6,17 +6,39 @@
 	</head>
 	<body>
 		<?php
-		echo 'Hello World';
-		echo 'hi there';
+                //echo 'Hello World';
+		//echo 'hi there';
                 $temp = 'Jim';
-                echo 'Hi, my name is';
-                echo $temp;
+                //echo 'Hi, my name is';
+                //echo $temp;
                 $temp = 'geek';
-                echo "I am a";
-                echo$temp;
+                //echo "I am a";
+                //echo$temp;
                 $temp = 10;
-                echo 'My level is';
-                echo $temp;
-		?>
+                //echo 'My level is';
+                //echo $temp;
+                
+                $name = 'Jim';
+                $what = 'geek';
+                $level = 10;
+                //echo 'Hi, my name is ' .$name. ', and I am a level ' .$level. ' ' .
+                //        $what;
+                
+                $hoursworked = $_GET['hours'];
+                $rate = 12;
+                
+                if($hoursworked > 40) {
+                    $total = $hoursworked * $rate * 1.5;
+                } else {
+                    $total = $hoursworked * $rate;
+                }
+                
+                //echo 'Hi, my name is ' .$name. ', and I am a level ' .$level. ' ' .
+                //        $what . '. ';
+                
+                //echo ($total > 0) ? 'You owe me ' . $total : "You're welcome";
+		
+                
+                ?>
 	</body>
 </html>
---
+++ tictactoe.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+                $position = $_GET['board'];
+                $squares = str_split($position);
+                
+                function winner($token, $position) {
+                    $won = false;
+                    if(($position[0] == $token) && 
+                       ($position[1] == $token) &&
+                       ($position[2] == $token)) {
+                        $won = true;
+                    } else if(($position[3] == $token) && 
+                       ($position[4] == $token) &&
+                       ($position[5] == $token)) {
+                        $won = true;
+                    } else if(($position[6] == $token) && 
+                       ($position[7] == $token) &&
+                       ($position[8] == $token)) {
+                        $won = true;
+                    } else if(($position[0] == $token) && 
+                       ($position[3] == $token) &&
+                       ($position[6] == $token)) {
+                        $won = true;
+                    } else if(($position[1] == $token) && 
+                       ($position[4] == $token) &&
+                       ($position[7] == $token)) {
+                        $won = true;
+                    } else if(($position[2] == $token) && 
+                       ($position[5] == $token) &&
+                       ($position[8] == $token)) {
+                        $won = true;
+                    } else if(($position[0] == $token) && 
+                       ($position[3] == $token) &&
+                       ($position[6] == $token)) {
+                        $won = true;
+                    } else if(($position[0] == $token) && 
+                       ($position[5] == $token) &&
+                       ($position[8] == $token)) {
+                        $won = true;
+                    } else if(($position[2] == $token) && 
+                       ($position[5] == $token) &&
+                       ($position[6] == $token)) {
+                        $won = true;
+                    }
+                    return $won;
+                }
+                
+                if (winner('x',$squares)) echo 'X, You win.';
+                else if (winner('o',$squares)) echo 'O, I win.';
+                else echo 'No winner yet.';
+                
+                ?>
+	</body>
+</html>
